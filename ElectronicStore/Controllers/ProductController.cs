26826c1bd0b6d66fc550ac2fe8e05e33e6e5bc98
Daniel Isaac <trebolman@gmail.com>
2018-12-15 15:36:59 -0500

    Controllers Creation and configuration

---
+++ ElectronicStore/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ElectronicStore.Models;
+
+namespace ElectronicStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public bool islog = false;
+        public ViewResult Index()
+        {   
+            ViewBag.log = islog ? true : false;
+            return View();
+        }
+
+        public ViewResult Login()
+        {
+            return View();
+        }
+
+        public ViewResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult AddProduct(Product producto)
+        {
+            return View();
+        }
+    }
+}
